63d344b97722dfc5151cb45490f4357c570b1083
jerrynabango <jerrynabango9@gmail.com>
2023-08-01 11:39:37 +0300

    matching of previous char

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -4,7 +4,7 @@
 # /h, b & t: The chararacters to search
 # .: Search all the matching characters apart from newline characters
 # scan: searches the required characters in the search string
-# ?: Matches zero or one occurrence of the previous character(b).
+# ?: matches zero or one occurrence of the previous character(b).
 # join: Provides convenient join for the search string and returns the result
 
 puts ARGV[0].scan(/hb?tn/).join
