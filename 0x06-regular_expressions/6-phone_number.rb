ba9d0ac51a1876f9f02323d24114c80b5c5d42ec
jerrynabango <jerrynabango9@gmail.com>
2023-08-01 16:15:23 +0300

    display n.o using metachar {} and \d

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -6,7 +6,7 @@
 # ^: Matches the start of a line(h).
 # \d: Matches any digit character. It is equivalent to [0-9].
 # {}: Matches exactly 10 occurrences of the previous character or group.
-# $: Matches the end of a line(n).
+# $: Matches the end of a line.
 # join: Provides convenient join for the search string and returns the result
 
 puts ARGV[0].scan(^\d{10})$/.join
