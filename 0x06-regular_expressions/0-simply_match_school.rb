0cdb38d5f1cf130e7369ac5cf3a4a4ff58fa28b3
jerrynabango <jerrynabango9@gmail.com>
2023-08-01 11:15:24 +0300

    matching school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -4,4 +4,5 @@
 # scan: searches the required characters in the search string
 # school: The chararacters to search
 # join: Provides a convenient join for the search string and returns the result
+
 puts ARGV[0].scan(/School/).join
