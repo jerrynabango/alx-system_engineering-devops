b93ebf1bd6d910c5782f9785679a65ed0e34600e
jerrynabango <jerrynabango9@gmail.com>
2023-08-01 15:54:09 +0300

    repetition using metachar +

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -3,7 +3,7 @@
 # puts: Dislpays the searched list
 # .: Search all the matching characters apart from newline characters
 # scan: searches the required characters in the search string
-# +: Matches one or more occurrences of the previous character(b).
+# +: Matches one or more occurrences of the previous character(t).
 # join: Provides convenient join for the search string and returns the result
 
 puts ARGV[0].scan(/hbt+n/).join
