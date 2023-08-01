899965d535df706c9215f886c28be89f5c5294a2
jerrynabango <jerrynabango9@gmail.com>
2023-08-01 15:57:27 +0300

    check char least & most occurrence using metachar {}

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -4,7 +4,7 @@
 # /h, b & t: The chararacters to search
 # .: Search all the matching characters apart from newline characters
 # scan: searches the required characters in the search string
-# {2,5}: Matches at least 2 and at most 5 occurrences of the previous character or group.
+# {2,5}: Matches at least 2 and at most 5 occurrences of the previous character(t).
 # join: Provides convenient join for the search string and returns the result
 
 puts ARGV[0].scan(/hbt{2,5}n/).join
