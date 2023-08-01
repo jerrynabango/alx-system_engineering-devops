667f79c29fc278c209c60791e5938d3159e1040d
jerrynabango <jerrynabango9@gmail.com>
2023-08-01 16:14:59 +0300

    display n.o using metachar {} and \d

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+# puts: Dislpays the searched list
+# .: Search all the matching characters apart from newline characters
+# scan: searches the required characters in the search string
+# ^: Matches the start of a line(h).
+# \d: Matches any digit character. It is equivalent to [0-9].
+# {}: Matches exactly 10 occurrences of the previous character or group.
+# $: Matches the end of a line(n).
+# join: Provides convenient join for the search string and returns the result
+
+puts ARGV[0].scan(^\d{10})$/.join
