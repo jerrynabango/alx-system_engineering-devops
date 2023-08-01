247adf3b3520eb31b750533542e47334e38b57bc
jerrynabango <jerrynabango9@gmail.com>
2023-08-01 15:30:30 +0300

    repettion using metachar ?

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
 # puts: Dislpays the searched list
-# /h, b & t: The chararacters to search
 # .: Search all the matching characters apart from newline characters
 # scan: searches the required characters in the search string
 # ?: matches zero or one occurrence of the previous character(b).
