34ea4363efbd30fc1a9321dc349c9f22e4183b81
jerrynabango <jerrynabango9@gmail.com>
2023-08-01 15:29:57 +0300

    repettion using metachar +

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+# puts: Dislpays the searched list
+# .: Search all the matching characters apart from newline characters
+# scan: searches the required characters in the search string
+# +: Matches one or more occurrences of the previous character(b).
+# join: Provides convenient join for the search string and returns the result
+
+puts ARGV[0].scan(/hb+tn/).join
