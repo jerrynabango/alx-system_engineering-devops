0168ff619994f69d7af9a099ca6e1ae9c593d036
jerrynabango <jerrynabango9@gmail.com>
2023-08-01 11:17:46 +0300

    matching school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 # puts: Dislpays the searched list
+# .: Search all the matching characters apart from newline characters
 # scan: searches the required characters in the search string
 # school: The chararacters to search
 # join: Provides a convenient join for the search string and returns the result
