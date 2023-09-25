e70f632c0470e83ce0a5fc8eca9a11f2fdbff5a6
jerrynabango <jerrynabango9@gmail.com>
2023-09-25 09:16:07 +0300

    creating pp file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# Create a file
+
 file { '/tmp/school':
-path => '/tmp/school',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-contents => 'I love Puppet'
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
