906777920a4ffad31a743b3b78ee94457c5aec8c
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 16:53:28 +0300

    Records all tasks from all employees

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,20 +2,28 @@
 """
 Records all tasks from all employees
 """
+
 import json
 import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     employees_tasks = requests.get(url + "employees_tasks").json()
-
     with open("todo_all_employees.json", 'w') as jsonfile:
-        #json.dump: Used to write to python fie to JSON file format
-        json.dump({
-            all_tasks.get("id"): [{
-                "task": records.get("title"),
-                "completed": records.get("completed"),
-                "username": all_tasks.get("username")
-            } for records in requests.get(url + "todos",
-                                    params={"userId": all_tasks.get("id")}).json()]
-            for all_tasks in employees_tasks}, jsonfile)
+        json.dump(
+            {
+                all_tasks.get("id"): [
+                    {
+                        "task": records.get("title"),
+                        "completed": records.get("completed"),
+                        "username": all_tasks.get("username")
+                    }
+                    for records in requests.get(
+                        url + "todos",
+                        params={"userId": all_tasks.get("id")}
+                    ).json()
+                ]
+                for all_tasks in employees_tasks
+            },
+            jsonfile
+        )
