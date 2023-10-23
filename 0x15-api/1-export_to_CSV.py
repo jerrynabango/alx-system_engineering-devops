90b06030df24d6f37ef241511d568ce6435f2a92
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 15:55:18 +0300

    gather data from api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 """
-Records all tasks that are owned by this employee
+Export to CSV
 """
 
 import sys
 import requests
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+import requests
+import sys
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+
+def export_to_csv(uid):
+    """
+    Records all tasks that are owned by this employee
+    """
+    base = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(base + "users/" + uid).json()
+    userTodos = requests.get(base + "todos", params={"userId": uid}).json()
+    with open("{}.csv".format(uid), 'w', encoding="utf-8") as w:
+        for _ in userTodos:
+            row = '"{}","{}","{}","{}"\n'.format(
+                uid, user.get("username"), _.get("completed"), _.get("title"))
+            w.write(row)
+
+
+if __name__ == "__main__":
+    export_to_csv(sys.argv[1])
