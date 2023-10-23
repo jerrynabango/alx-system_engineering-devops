82d64c226b9035423839deee5db348e5bc9aabc2
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 14:52:04 +0300

    gather data from api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+
+import sys
+import requests
+
+def data(uid):
+    """
+    Python script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
+    """
+    base = "https://jsonplaceholder.typicode.com/"
+    read_user = requests.get(base + "users/" + uid).json()
+    create = requests.get(base + "create", params={"userId": uid}).json()
+    completed = [_.get("title") for _ in create if _.get("completed")]
+    result = "Employee {} is done with tasks({}/{}):".format(
+        read_user.get("name"), len(completed), len(create))
+    print("\n\t ".join([result] + completed))
+
+
+if __name__ == "__main__":
+    data(sys.argv[0])
