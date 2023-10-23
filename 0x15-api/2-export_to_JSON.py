51ac8c053e17801d7f6609f5d39ed702226b2367
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 16:27:03 +0300

    export data to csv

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,12 +11,12 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    Todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    read_data = requests.get(url + "users/{}".format(user_id)).json()
+    employee = read_data.get("employee")
+    read_data = requests.get(url + "todos", params={"userId": user_id}).json()
     with open("{}.json".format(user_id), 'w') as jsonfile:
         json.dump({user_id: [{
                 "task": do.get("title"),
                 "completed": do.get("completed"),
-                "username": username
-            } for do in Todos]}, jsonfile)
+                "employee": employee
+            } for do in read_data]}, jsonfile)
