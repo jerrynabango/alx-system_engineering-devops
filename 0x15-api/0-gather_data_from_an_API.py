a992ca20d31b555dd4c1850c13d2f0165bbea34e
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 15:01:32 +0300

    gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """
-Gather data from an API
+Python script that, using the JSON PLACEHOLDER API,
+for a given employee ID, returns information about his/her TODO list progress
 """
-
-import sys
 import requests
+import sys
 
 
-def data(uid):
+def get_data_from_api(uid):
     """
-    Python script that, using this REST API, for a given employee ID,
-    returns information about his/her TODO list progress.
+    Gets and prints data from JSON PLACEHOLDER API
+    Args:
+        uid: employee id
+    Return:
+        None
     """
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    Todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in Todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(Todos)))
-    [print("\t {}".format(c)) for c in completed]
+    base = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(base + "users/" + uid).json()
+    userTodos = requests.get(base + "todos", params={"userId": uid}).json()
+    completed = [_.get("title") for _ in userTodos if _.get("completed")]
+    output = "Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(userTodos))
+    print("\n\t ".join([output] + completed))
 
 
 if __name__ == "__main__":
-    data(sys.argv[1])
+    get_data_from_api(sys.argv[1])
