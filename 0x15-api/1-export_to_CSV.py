fa48455463e478e84cda1e30ba42bbdbfc7b0998
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 16:00:21 +0300

    gather data from api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,10 +13,10 @@ def data(uid):
     """
     base = "https://jsonplaceholder.typicode.com/"
     user = requests.get(base + "users/" + uid).json()
-    userTodos = requests.get(base + "todos", params={"userId": uid}).json()
+    todos = requests.get(base + "todos", params={"userId": uid}).json()
 
     with open("{}.csv".format(uid), 'w') as w:
-        for _ in userTodos:
+        for _ in todos:
             row = '"{}","{}","{}","{}"\n'.format(
                 uid, user.get("username"), _.get("completed"), _.get("title"))
             w.write(row)
