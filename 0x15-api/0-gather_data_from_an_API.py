428a75c0c59dbd8066aabf520530c153bc0839a5
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 16:30:12 +0300

    gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,23 +3,23 @@
 Gather data from an API
 """
 
-import sys
 import requests
+import sys
 
 
-def get_data_from_api(uid):
+def employee_info(uid):
     """
     Python script that, using this REST API, for a given employee ID,
     returns information about his/her TODO list progress.
     """
     base = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(base + "users/" + uid).json()
-    userTodos = requests.get(base + "todos", params={"userId": uid}).json()
-    completed = [_.get("title") for _ in userTodos if _.get("completed")]
-    output = "Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(userTodos))
-    print("\n\t ".join([output] + completed))
+    employee = requests.get(base + "users/" + uid).json()
+    read_data = requests.get(base + "todos", params={"userId": uid}).json()
+    completed = [_.get("title") for _ in read_data if _.get("completed")]
+    details = "Employee {} is done with tasks({}/{}):".format(
+        employee.get("name"), len(completed), len(read_data))
+    print("\n\t ".join([details] + completed))
 
 
 if __name__ == "__main__":
-    get_data_from_api(sys.argv[1])
+    employee_info(sys.argv[1])
