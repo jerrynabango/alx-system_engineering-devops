dc549b945e158c68e90253b794836f8042128dbc
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 16:08:06 +0300

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
-                "task": To.get("title"),
+                "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": username
             } for t in todos]}, jsonfile)
