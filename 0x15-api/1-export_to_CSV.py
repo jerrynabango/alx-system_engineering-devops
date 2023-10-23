47d8aec9a5d55a4e35924ec51863cbf566bbb3c0
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 16:21:55 +0300

    export data to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,13 +12,12 @@ def data(uid):
     Records all tasks that are owned by this employee
     """
     base = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(base + "users/" + uid).json()
-    todos = requests.get(base + "todos", params={"userId": uid}).json()
-
+    employee = requests.get(base + "users/" + uid).json()
+    Todos = requests.get(base + "todos", params={"userId": uid}).json()
     with open("{}.csv".format(uid), 'w') as w:
-        for _ in todos:
+        for _ in Todos:
             row = '"{}","{}","{}","{}"\n'.format(
-                uid, user.get("username"), _.get("completed"), _.get("title"))
+                uid, employee.get("username"), _.get("completed"), _.get("title"))
             w.write(row)
 
 
