e89020e3a71e3acd780a8658b737738db3561251
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 15:49:09 +0300

    export data to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,10 +13,10 @@ def employee_data(uid):
     """
     base = "https://jsonplaceholder.typicode.com/"
     user = requests.get(base + "users/" + uid).json()
-    todos = requests.get(base + "todos", params={"userId": uid}).json()
-    
-    with open("{}.csv".format(uid), "w") as w:
-        for _ in todos:
+    userTodos = requests.get(base + "todos", params={"userId": uid}).json()
+
+    with open("{}.csv".format(uid), 'w') as w:
+        for _ in userTodos:
             row = '"{}","{}","{}","{}"\n'.format(
                 uid, user.get("username"), _.get("completed"), _.get("title"))
             w.write(row)
