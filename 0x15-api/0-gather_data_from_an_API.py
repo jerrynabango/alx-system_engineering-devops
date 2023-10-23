049905ed7f7fc5c5e7d7037b9a78e872c6daab97
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 15:25:36 +0300

    gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,9 +16,9 @@ def data(uid):
     user = requests.get(base + "users/" + uid).json()
     todos = requests.get(base + "todos", params={"userId": uid}).json()
     completed = [_.get("title") for _ in todos if _.get("completed")]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    output = "Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos))
+    print("\n\t ".join([output] + completed))
 
 
 if __name__ == "__main__":
