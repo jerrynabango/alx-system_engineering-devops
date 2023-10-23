ba8d6a21a89ccb48599f71f8432dbb58e3e907ce
jerrynabango <jerrynabango9@gmail.com>
2023-10-23 15:45:18 +0300

    gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,19 +7,12 @@ import sys
 import requests
 
 
-def get_data_from_api(uid):
-    """
-    Python script that, using this REST API, for a given employee ID,
-    returns information about his/her TODO list progress.
-    """
-    base = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(base + "users/" + uid).json()
-    userTodos = requests.get(base + "todos", params={"userId": uid}).json()
-    completed = [_.get("title") for _ in userTodos if _.get("completed")]
-    output = "Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(userTodos))
-    print("\n\t ".join([output] + completed))
-
-
 if __name__ == "__main__":
-    get_data_from_api(sys.argv[0])
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
