23e303043e2bac8817974dd4a7899606d84a9764
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 16:23:55 +0300

    recursion

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,12 +11,12 @@ def recurse(subreddit, hot_list=[]):
     returns a list containing the titles of all hot
     articles for a given subreddit, else return None.
     """
-    headers = {'User-Agent': 'advanced-api/0.0.1 by User'}
+    user_agent = {'User-Agent': 'api_advanced'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     global after
 
     parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=headers,
+    results = requests.get(url, params=parameters, headers=user_agent,
                            allow_redirects=False)
 
     if results.status_code == 200:
@@ -25,8 +25,8 @@ def recurse(subreddit, hot_list=[]):
             after = list
             recurse(subreddit, hot_list)
         titles = results.json().get("data").get("children")
-        for title_ in titles:
-            hot_list.append(title_.get("data").get("title"))
+        for title in titles:
+            hot_list.append(title.get("data").get("title"))
         return hot_list
     else:
         return (None)
