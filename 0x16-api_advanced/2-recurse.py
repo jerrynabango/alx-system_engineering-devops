ff60d2fe596ab18872f1512fc8ecd08b399f59c6
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 16:13:28 +0300

    recursion

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,16 +12,16 @@ def recurse(subreddit, hot_list=[]):
     articles for a given subreddit, else return None.
     """
     global after
-    headers = {'User-Agent': 'api_advanced-project'}
+    user_agent = {'User-Agent': 'api_advanced-project'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=headers,
+    results = requests.get(url, params=parameters, headers=user_agent,
                            allow_redirects=False)
 
     if results.status_code == 200:
-        after_data = results.json().get("data").get("after")
-        if after_data is not None:
-            after = after_data
+        list = results.json().get("data").get("after")
+        if list is not None:
+            after = list
             recurse(subreddit, hot_list)
         all_titles = results.json().get("data").get("children")
         for title_ in all_titles:
