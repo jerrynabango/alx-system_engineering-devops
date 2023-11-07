13c42b37d7cd00409400d1ea91b8c5e3954496ae
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 14:00:07 +0300

    count keywords from the articles

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     (not active users, total subscribers) for a given subreddit,
     else returns None.
     """
-    subscribers = 0
+    number_subscribers = 0
 
     headers = {'User-Agent': 'advanced-api/0.0.1 by MyName'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
@@ -18,5 +18,5 @@ def number_of_subscribers(subreddit):
 
     if request.status_code == 200:
         response = request.json()
-        subscribers = response['data']['subscribers']
-    return subscribers
+        number_subscribers = response['data']['number_subscribers']
+    return number_subscribers
