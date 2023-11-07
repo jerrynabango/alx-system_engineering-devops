a6ae1a60b7236b4604031386d481488a63e37814
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 13:15:39 +0300

    count keywords from the articles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-"""Count keywords from articles"""
-
+"""
+Defines a function that queries Reddit API
+"""
 import requests
 
 
 def count_words(subreddit, word_list, after=None, sort=True):
     """
-    Function that queries the Reddit API, parses the title of all hot articles,
-    & prints a sorted articles_count of given keywords.
+    Queries the Reddit API, parses the title of all hot articles,
+    and prints a sorted count of given keywords (case-insensitive, delimited
+    by spaces. Javascript should count as javascript, but java should not)
+    Ags:
+        subreddit (str): name of subreddit
+        word_list (list): keywords to look out for
+        after (str): identifier of the last item on a listing
+        worddict (dict): results to be returned
+        ctr (int): condition to convert word_list to worddict
+    Returns:
+        worddict (dict) || None if subreddit is invalid
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    parameter = {'after': after, 'limit': 100}
-    custom = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
-    respionse = requests.get(url=url, parameter=parameter, custom=custom,
-                             allow_redirects=False)
-    if respionse.status_code == 200:
-        response = respionse.json()
+    params = {'after': after, 'limit': 100}
+    headers = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
+    req = requests.get(url=url,
+                       params=params, headers=headers, allow_redirects=False)
+    if req.status_code == 200:
+        response = req.json()
         titles = [child['data']['title']
                   for child in response['data']['children']]
         after = response['data']['after']
@@ -23,20 +33,16 @@ def count_words(subreddit, word_list, after=None, sort=True):
             titles += count_words(subreddit,
                                   word_list, after=after, sort=False)
         if sort is True:
-            articles_count = {key.lower(): 0 for key in word_list}
+            count = {k.lower(): 0 for k in word_list}
             for title in titles:
-                articles_count = {key: value + title.lower().split()
-                                  .articles_count(key)
-                                  for key, value in articles_count.items()}
-            articles_count = {key: value for key,
-                              value in articles_count.items() if value > 0}
-            if len(articles_count):
+                count = {k: v + title.lower().split().count(k)
+                         for k, v in count.items()}
+            count = {k: v for k, v in count.items() if v > 0}
+            if len(count):
                 word_list = [w.lower() for w in word_list]
-                articles_count = {key: value * word_list.articles_count(key)
-                                  for key, value in articles_count.items()}
-                articles_count = sorted(articles_count.items(),
-                                        key=lambda all: (-all[1], all[0]))
-                [print("{}: {}".format(key, value))
-                 for key, value in articles_count]
+                count = {k: v * word_list.count(k)
+                         for k, v in count.items()}
+                count = sorted(count.items(), key=lambda kv: (-kv[1], kv[0]))
+                [print("{}: {}".format(k, v)) for k, v in count]
         else:
             return titles
