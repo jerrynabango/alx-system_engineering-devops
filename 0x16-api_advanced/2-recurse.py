2e1c6d79a40ed4221636082dbe6702b5721abc24
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 16:10:42 +0300

    recursion

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,10 +7,13 @@ after = None
 
 
 def recurse(subreddit, hot_list=[]):
-    """returning top ten post titles recursively"""
+    """Function that queries the Reddit API &
+    returns a list containing the titles of all hot
+    articles for a given subreddit, else return None.
+    """
+    global after
     user_agent = {'User-Agent': 'api_advanced-project'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    global after
     parameters = {'after': after}
     results = requests.get(url, params=parameters, headers=user_agent,
                            allow_redirects=False)
@@ -20,8 +23,8 @@ def recurse(subreddit, hot_list=[]):
         if after_data is not None:
             after = after_data
             recurse(subreddit, hot_list)
-        titles = results.json().get("data").get("children")
-        for title_ in titles:
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
             hot_list.append(title_.get("data").get("title"))
         return hot_list
     else:
