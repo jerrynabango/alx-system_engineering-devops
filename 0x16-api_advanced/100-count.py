127c2627f49048a00be24d10b01f3ae2c3777541
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 16:42:39 +0300

    count keywords from the articles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -24,16 +24,16 @@ def count_words(subreddit, word_list, after=None, sort=True):
             titles += count_words(subreddit,
                                   word_list, after=after, sort=False)
         if sort is True:
-            count = {k.lower(): 0 for k in word_list}
+            count = {key.lower(): 0 for key in word_list}
             for title in titles:
-                count = {k: v + title.lower().split().count(k)
-                         for k, v in count.items()}
-            count = {k: v for k, v in count.items() if v > 0}
+                count = {key: value + title.lower().split().count(key)
+                         for key, value in count.items()}
+            count = {key: value for key, value in count.items() if value > 0}
             if len(count):
-                word_list = [w.lower() for w in word_list]
-                count = {k: v * word_list.count(k)
-                         for k, v in count.items()}
+                word_list = [word.lower() for word in word_list]
+                count = {key: value * word_list.count(key)
+                         for key, value in count.items()}
                 count = sorted(count.items(), key=lambda kv: (-kv[1], kv[0]))
-                [print("{}: {}".format(k, v)) for k, v in count]
+                [print("{}: {}".format(key, value)) for key, value in count]
         else:
             return titles
