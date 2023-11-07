a7e05a5733d479e91eb312109e02df64c837d115
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 13:14:25 +0300

    count keywords from the articles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Count keywords """
+"""Count keywords from articles"""
 
 import requests
 
@@ -7,18 +7,15 @@ import requests
 def count_words(subreddit, word_list, after=None, sort=True):
     """
     Function that queries the Reddit API, parses the title of all hot articles,
-    and prints sorted title_count of given keywords.
+    & prints a sorted articles_count of given keywords.
     """
-    if not subreddit or not isinstance(subreddit, str):
-        return None
-
-    custom = {'User-Agent': 'advanced-api/0.0.1 by MyName'}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     parameter = {'after': after, 'limit': 100}
-    response = requests.get(url=url, parameter=parameter,
-                            custom=custom, allow_redirects=False)
-    if response.status_code == 200:
-        response = response.json()
+    custom = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
+    respionse = requests.get(url=url, parameter=parameter, custom=custom,
+                             allow_redirects=False)
+    if respionse.status_code == 200:
+        response = respionse.json()
         titles = [child['data']['title']
                   for child in response['data']['children']]
         after = response['data']['after']
@@ -26,20 +23,20 @@ def count_words(subreddit, word_list, after=None, sort=True):
             titles += count_words(subreddit,
                                   word_list, after=after, sort=False)
         if sort is True:
-            title_count = {key.lower(): 0 for key in word_list}
+            articles_count = {key.lower(): 0 for key in word_list}
             for title in titles:
-                title_count = {key: value + title.lower()
-                               .split().title_count(key)
-                               for key, value in title_count.items()}
-            title_count = {key: value for key, value in title_count.items()
-                           if value > 0}
-            if len(title_count):
+                articles_count = {key: value + title.lower().split()
+                                  .articles_count(key)
+                                  for key, value in articles_count.items()}
+            articles_count = {key: value for key,
+                              value in articles_count.items() if value > 0}
+            if len(articles_count):
                 word_list = [w.lower() for w in word_list]
-                title_count = {key: value * word_list.title_count(key)
-                               for key, value in title_count.items()}
-                title_count = sorted(title_count.items(),
-                                     key=lambda kv: (-kv[1], kv[0]))
+                articles_count = {key: value * word_list.articles_count(key)
+                                  for key, value in articles_count.items()}
+                articles_count = sorted(articles_count.items(),
+                                        key=lambda all: (-all[1], all[0]))
                 [print("{}: {}".format(key, value))
-                 for key, value in title_count]
+                 for key, value in articles_count]
         else:
             return titles
