c55d9038356b0df817460295d6c68b117ee1693c
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 14:24:37 +0300

    prints top ten titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,12 +9,12 @@ def top_ten(subreddit):
     Function that queries Reddit API & prints the titles of the first 10
     hot posts listed for a given subreddit.
     """
-    headers = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    request = requests.get(url=url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'advanced-api/0.0.1 by MyName'}
+    req = requests.get(url=url, headers=headers, allow_redirects=False)
 
-    if request.status_code == 200:
-        response = request.json()
+    if req.status_code == 200:
+        response = req.json()
         titles = [child['data']['title']
                   for child in response['data']['children'][:10]]
         for title in titles:
