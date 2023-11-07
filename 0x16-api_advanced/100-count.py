d59f849959f6b0085abaadd8aca17abf8d4d524f
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 17:14:49 +0300

    count keywords from the articles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,7 @@ import requests
 def count_words(subreddit, word_list, after=None, sort=True):
     """
     Queries the Reddit API, parses the title of all hot articles,
-    and prints a sorted count of given keywords (case-insensitive, delimited
+    & prints a sorted count of given keywords (case-insensitive, delimited
     by spaces.
     """
     user_agent = {'User-Agent': 'api_advanced'}
