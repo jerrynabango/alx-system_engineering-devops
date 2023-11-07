269b6ce26b8bd163b4d0726c15b6816d8567d32a
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 13:54:44 +0300

    count keywords from the articles

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,15 +6,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    If an invalid subreddit is given, the funcrtion returns None./
+    Function that queries reddit API & returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit,
+    else returns None.
     """
-    check_subscribers = 0
+    subscribers = 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
+    headers = {'User-Agent': 'advanced-api/0.0.1 by MyName'}
     req = requests.get(url=url, headers=headers, allow_redirects=False)
+
     if req.status_code == 200:
         response = req.json()
-        check_subscribers = response['data']['check_subscribers']
-    return check_subscribers
+        subscribers = response['data']['subscribers']
+    return subscribers
