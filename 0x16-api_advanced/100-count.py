6c2186e76a1ade291602628a6e00eeca9a508971
jerrynabango <jerrynabango9@gmail.com>
2023-11-07 13:17:16 +0300

    count keywords from the articles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -20,12 +20,12 @@ def count_words(subreddit, word_list, after=None, sort=True):
         worddict (dict) || None if subreddit is invalid
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    params = {'after': after, 'limit': 100}
-    headers = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
-    req = requests.get(url=url,
-                       params=params, headers=headers, allow_redirects=False)
-    if req.status_code == 200:
-        response = req.json()
+    parameter = {'after': after, 'limit': 100}
+    custom = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
+    result = requests.get(url=url,
+                       parameter=parameter, custom=custom, allow_redirects=False)
+    if result.status_code == 200:
+        response = result.json()
         titles = [child['data']['title']
                   for child in response['data']['children']]
         after = response['data']['after']
@@ -33,16 +33,16 @@ def count_words(subreddit, word_list, after=None, sort=True):
             titles += count_words(subreddit,
                                   word_list, after=after, sort=False)
         if sort is True:
-            count = {k.lower(): 0 for k in word_list}
+            count = {key.lower(): 0 for key in word_list}
             for title in titles:
-                count = {k: v + title.lower().split().count(k)
-                         for k, v in count.items()}
-            count = {k: v for k, v in count.items() if v > 0}
+                count = {key: value + title.lower().split().count(key)
+                         for key, value in count.items()}
+            count = {key: value for key, value in count.items() if value > 0}
             if len(count):
                 word_list = [w.lower() for w in word_list]
-                count = {k: v * word_list.count(k)
-                         for k, v in count.items()}
+                count = {key: value * word_list.count(key)
+                         for key, value in count.items()}
                 count = sorted(count.items(), key=lambda kv: (-kv[1], kv[0]))
-                [print("{}: {}".format(k, v)) for k, v in count]
+                [print("{}: {}".format(key, value)) for key, value in count]
         else:
             return titles
